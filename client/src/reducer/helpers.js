7458aeac8e45a735b858db573bfb0ee0082330d8
Willi Raschkowski <willi@raschkowski.com>
2017-04-01 02:57:03 +0200

    Don't import entire lodash

--- client/src/reducer/helpers.js
+++ client/src/reducer/helpers.js
@@ -1,18 +1,19 @@
-import * as _ from 'lodash';
-
+import { uniqBy } from 'lodash';
 
 export function linkArtist(tracks) {
-    const accumulator = ({ artists, links }, track) => {
-        links.push(...track.artists.map(a => ({ source: track.id, target: a.id })));
-        artists.push(...track.artists);
-        return { artists, links };
-    };
-    const { artists, links } = tracks.reduce(
-        accumulator,
-        { artists: [], links: [] }
-    );
-    return {
-        artists: _.uniqBy(artists, n => n.id),
-        links: _.uniqBy(links, l => l.source + l.target),
-    }
+  const accumulator = ({ artists, links }, track) => {
+    links.push(...track.artists.map(a => ({ source: track.id, target: a.id })));
+    artists.push(...track.artists);
+    return { artists, links };
+  };
+
+  const { artists, links } = tracks.reduce(
+    accumulator,
+    { artists: [], links: [] }
+  );
+
+  return {
+    artists: uniqBy(artists, n => n.id),
+    links: uniqBy(links, l => l.source + l.target),
+  };
 }
