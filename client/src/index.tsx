383aa0d969c11c2f2f6ab49517d0bcdea26585f8
Willi Raschkowski <willi@raschkowski.com>
2017-10-19 16:40:00 +0200

    Don't import styles in html

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
 import App from './components/App';
+import Error from './components/Error';
 import Login from './components/Login';
 import Songs from './components/Songs';
-import Error from './components/Error';
-import SpotifyData from './reducer/reducer';
 import './main.scss';
+import SpotifyData from './reducer/reducer';
 
 const store = createStore(SpotifyData, applyMiddleware(thunkMiddleware));
 
@@ -19,10 +19,10 @@ class Root extends React.Component {
       <Provider store={store}>
         <Router history={hashHistory}>
           <Route path="/" component={App}>
-            <IndexRoute component={Login}/>
-            <Route path="/login/:accessToken" component={Login}/>
-            <Route path="/error/:error" component={Error}/>
-            <Route path="/songs" component={Songs}/>
+            <IndexRoute component={Login} />
+            <Route path="/login/:accessToken" component={Login} />
+            <Route path="/error/:error" component={Error} />
+            <Route path="/songs" component={Songs} />
           </Route>
         </Router>
       </Provider>
@@ -30,4 +30,4 @@ class Root extends React.Component {
   }
 }
 
-render(<Root/>, document.getElementById('root'));
+render(<Root />, document.getElementById('root'));
