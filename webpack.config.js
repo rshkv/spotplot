eac90f0d06bf9a49d68e22612f77ea782245994f
Willi Raschkowski <willi@raschkowski.com>
2017-10-19 16:22:26 +0200

    Remove dependency optimization for now

--- webpack.config.js
+++ webpack.config.js
@@ -31,13 +31,12 @@ module.exports = {
   // assume a corresponding global variable exists and use that instead.
   // This is important because it allows us to avoid bundling all of our
   // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
-    "react": "React",
-    "react-dom": "ReactDOM"
-  },
+  // externals: {
+  //   "react": "React",
+  //   "react-dom": "ReactDOM"
+  // },
 
   node: {
-    // console: true,
     fs: 'empty',
     net: 'empty',
     tls: 'empty',
