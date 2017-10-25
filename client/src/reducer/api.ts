e24db3bccb5726225adf5c5353262f6b5cb466fd
Willi Raschkowski <willi@raschkowski.com>
2017-10-25 04:22:21 +0200

    Turn api into a class

--- client/src/reducer/api.ts
+++ client/src/reducer/api.ts
@@ -2,103 +2,116 @@ import * as _ from 'lodash';
 import * as rp from 'request-promise';
 import * as errors from 'request-promise/errors'; // tslint:disable-line no-submodule-imports
 
-/**
- * Load all tracks saved in user library.
- * @param accessToken Valid Spotify access token associated with user.
- */
-export async function getSavedTracks(accessToken: string): Promise<SpotifyApi.TrackObjectFull[]> {
-    const limit = 50;
-    const options = {
-        uri: 'https://api.spotify.com/v1/me/tracks',
-        headers: { Authorization: `Bearer ${accessToken}` },
-        json: true,
-        qs: { limit },
-    };
+export default class SpotifyApi {
+    private accessToken: string;
 
-    // Get first 50 items
-    const { total, items } = await rp(options);
-    // Build requests for remaining (total - 50) items
-    const requests = _.range(limit, total, limit)
-        .map(offset => rp({ ...options, qs: { limit, offset } }));
-    // Await all requests and map to tracks
-    const responses = (await Promise.all(requests)) as SpotifyApi.UsersSavedTracksResponse[];
-    return [...items, ..._.flatMap(responses, 'items')]
-        .map(i => i.track);
+    /**
+     * Create a new Api object.
+     * @param accessToken Valid Spotify access token.
+     */
+    constructor(accessToken?: string) {
+        this.accessToken = accessToken;
+    }
+
+    /**
+     * Set access token.
+     * @param accessToken Valid Spotify access token.
+     */
+    public setToken(accessToken: string): SpotifyApi {
+        this.accessToken = accessToken;
+        return this;
+    }
+
+    /** Load all tracks saved in user library. */
+    public async getSavedTracks(): Promise<SpotifyApi.TrackObjectFull[]> {
+        const limit = 50;
+        const options = {
+            uri: 'https://api.spotify.com/v1/me/tracks',
+            headers: { Authorization: `Bearer ${this.accessToken}` },
+            json: true,
+            qs: { limit },
+        };
+
+        // Get first 50 items
+        const { total, items } = await rp(options);
+        // Build requests for remaining (total - 50) items
+        const requests = _.range(limit, total, limit)
+            .map(offset => rp({ ...options, qs: { limit, offset } }));
+        // Await all requests and map to tracks
+        const responses = (await Promise.all(requests)) as SpotifyApi.UsersSavedTracksResponse[];
+        return [...items, ..._.flatMap(responses, 'items')]
+            .map(i => i.track);
+    }
+
+    /**
+     * Load artists for a list of ids.
+     * @param ids List of artist ids.
+     */
+    public async getArtists(ids: string[]): Promise<SpotifyApi.ArtistObjectFull[]> {
+        const limit = 50;
+        const options = {
+            uri: 'https://api.spotify.com/v1/artists',
+            headers: { Authorization: `Bearer ${this.accessToken}` },
+            json: true,
+        };
+
+        // Chunk ids to fit the limit and build request per chunk
+        const requests = _(ids)
+            .chunk(limit)
+            .map(chunk => rp({ ...options, qs: { ids: chunk.join(',') } }))
+            .value();
+
+        const responses = (await Promise.all(requests)) as SpotifyApi.MultipleArtistsResponse[];
+        return _.flatMap(responses, 'artists');
+    }
+    /**
+     * Load full artists for tracks.
+     * @param tracks List of artist ids.
+     */
+    public async getArtistsFromTracks(
+        tracks: SpotifyApi.TrackObjectFull[] | SpotifyApi.TrackObjectSimplified[]
+    ): Promise<SpotifyApi.ArtistObjectFull[]> {
+        // Get a all unique artist ids
+        const artistIds = _(tracks)
+            .flatMap(t => _.map(t.artists, 'id'))
+            .uniq()
+            .value();
+
+        return await this.getArtists(artistIds);
+    }
+
+    /**
+     * Play a track.
+     * @param trackUri Spotify uri of form 'spotify:track:abc123'
+     */
+    public async playTrack(trackUri: string) {
+        const response = await rp.put({
+            uri: 'https://api.spotify.com/v1/me/player/play',
+            headers: { Authorization: `Bearer ${this.accessToken}` },
+            json: true,
+            body: { uris: [trackUri] },
+            resolveWithFullResponse: true,
+        });
+
+        if (response.statusCode !== 204) throw new PlayError(response.statusCode);
+    }
+
+    /**
+     * Pause playback.
+     * @param trackUri Spotify uri of form 'spotify:track:abc123'
+     */
+    public async pause() {
+        const response = await rp.put({
+            uri: 'https://api.spotify.com/v1/me/player/pause',
+            headers: { Authorization: `Bearer ${this.accessToken}` },
+            resolveWithFullResponse: true,
+        });
+
+        if (response.statusCode !== 204) throw new PlayError(response.statusCode);
+    }
 }
 
-/**
- * Load artists for a list of ids.
- * @param ids List of artist ids.
- * @param accessToken Valid Spotify access token.
- */
-export async function getArtists(ids: string[], accessToken: string): Promise<SpotifyApi.ArtistObjectFull[]> {
-    const limit = 50;
-    const options = {
-        uri: 'https://api.spotify.com/v1/artists',
-        headers: { Authorization: `Bearer ${accessToken}` },
-        json: true,
-    };
-
-    // Chunk ids to fit the limit and build request per chunk
-    const requests = _(ids)
-        .chunk(limit)
-        .map(chunk => rp({ ...options, qs: { ids: chunk.join(',') } }))
-        .value();
-
-    const responses = (await Promise.all(requests)) as SpotifyApi.MultipleArtistsResponse[];
-    return _.flatMap(responses, 'artists');
-}
-
-/**
- * Load full artists for tracks.
- * @param tracks List of artist ids.
- * @param accessToken Valid Spotify access token.
- */
-export async function getArtistsFromTracks(
-    tracks: SpotifyApi.TrackObjectFull[] | SpotifyApi.TrackObjectSimplified[],
-    accessToken: string,
-): Promise<SpotifyApi.ArtistObjectFull[]> {
-    // Get a all unique artist ids
-    const artistIds = _(tracks)
-        .flatMap(t => _.map(t.artists, 'id'))
-        .uniq()
-        .value();
-
-    return await getArtists(artistIds, accessToken);
-}
-
-/**
- * Play a track.
- * @param trackUri Spotify uri of form 'spotify:track:abc123'
- * @param accessToken Spotify token with premium and 'user-modify-playback-state' permissions.
- */
-export async function playTrack(trackUri: string, accessToken: string) {
-    const response = await rp.put({
-        uri: 'https://api.spotify.com/v1/me/player/play',
-        headers: { Authorization: `Bearer ${accessToken}` },
-        json: true,
-        body: { uris: [trackUri] },
-        resolveWithFullResponse: true,
-    });
-
-    if (response.statusCode !== 204) throw new PlayError(response.statusCode);
-}
-
-/**
- * Pause playback.
- * @param trackUri Spotify uri of form 'spotify:track:abc123'
- * @param accessToken Spotify token with premium and 'user-modify-playback-state' permissions.
- */
-export async function pause(accessToken: string) {
-    const response = await rp.put({
-        uri: 'https://api.spotify.com/v1/me/player/pause',
-        headers: { Authorization: `Bearer ${accessToken}` },
-        resolveWithFullResponse: true,
-    });
-
-    if (response.statusCode !== 204) throw new PlayError(response.statusCode);
-}
-
+/* tslint:disable max-classes-per-file */
 class PlayError extends Error {
     public statusCode: number;
     constructor(statusCode: number) {
--- spec/api.spec.ts
+++ spec/api.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import 'mocha';
 import * as sinon from 'sinon';
-import * as api from '../client/src/reducer/api';
+import SpotifyApi from '../client/src/reducer/api';
 import { accessToken } from './token';
 
 (global as any).XMLHttpRequest = sinon.useFakeXMLHttpRequest();
@@ -11,12 +11,14 @@ function log(obj) {
     console.log(JSON.stringify(obj, null, 2));
 }
 
+const api = new SpotifyApi(accessToken);
+
 describe('Api', () => {
 
     it('should return saved tracks', async () => {
         const expectedLastSavedTrack = '3Nlq1iM31vOPARfaS43CUc';
 
-        const tracks = await api.getSavedTracks(accessToken);
+        const tracks = await api.getSavedTracks();
         const lastTrack = tracks[tracks.length - 1];
         expect(tracks.length).to.be.greaterThan(0);
         expect(lastTrack.id).to.equal(expectedLastSavedTrack);
@@ -26,7 +28,7 @@ describe('Api', () => {
     it('should return artists', async () => {
         const ids = ['0oSGxfWSnnOXhD2fKuz2Gy', '3dBVyJ7JuOMt4GE9607Qin'];
 
-        const artists = await api.getArtists(ids, accessToken);
+        const artists = await api.getArtists(ids);
         expect(artists.map(a => a.id)).to.have.ordered.members(ids);
         expect(artists[0]).to.have.property('popularity');
     });
