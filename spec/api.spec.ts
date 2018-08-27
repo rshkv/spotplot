772e0e1eabcff7aedbff7844ef93bbc8fa43d3a9
rshkv <willi@raschkowski.com>
2018-08-27 14:16:56 +0200

    Remove unused config files

--- spec/api.spec.ts
+++ spec/api.spec.ts
@@ -1,17 +1,24 @@
+/**
+ * A few tests of the client-side Api calls.
+ * These are integration tests and 'ACCESS_TOKEN' below must be set to run them.
+ * You can pull the token from local storage when running the app.
+ */
 import { expect } from 'chai';
 import 'mocha';
 import * as sinon from 'sinon';
 import Api from '../client/src/reducer/api';
-import { accessToken } from './token';
 
+// tslint:disable-next-line max-line-length
+const ACCESS_TOKEN = 'BQCphCq4bazTtA1krYmur5oDr12RPWyKvYbWyMUfIe6xb1Ke6wgEkFBoDf-gJ8To1J-f-5y0BmbPHoJiuFbQFxQQBge8-PdpMKlhSJaJ_YiYytHiOeBbnYIRG-2UlhughVnWTtiXT-H79iJrKzLvnVmEPM9vy5uGOm9d06N5UEP26fm-OqfDWIQX';
+// To run browser-like http requests in Node
 (global as any).XMLHttpRequest = sinon.useFakeXMLHttpRequest();
-
+// Utility to pretty-print request results
 function log(obj) {
     /* tslint:disable no-console */
     console.log(JSON.stringify(obj, null, 2));
 }
 
-const api = new Api(accessToken);
+const api = new Api(ACCESS_TOKEN);
 
 describe('Api', () => {
 
@@ -23,7 +30,7 @@ describe('Api', () => {
         expect(tracks.length).to.be.greaterThan(0);
         expect(lastTrack.id).to.equal(expectedLastSavedTrack);
         expect(lastTrack).to.have.property('popularity');
-    }).timeout(10000);
+    }).timeout(20000);
 
     it('should return artists', async () => {
         const ids = ['0oSGxfWSnnOXhD2fKuz2Gy', '3dBVyJ7JuOMt4GE9607Qin', '3dBVyJ7JuOMt4GE9607Qin'];
@@ -41,6 +48,6 @@ describe('Api', () => {
         const tracks = await api.getPlaylistTracks(user, playlist);
         expect(tracks.length).to.be.equal(200);
         expect(tracks[0].id).to.be.equal('48RvhoZ9cVHyEUaVGSi20C');
-    }).timeout(2000);
+    }).timeout(5000);
 
 });
